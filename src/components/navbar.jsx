82cf9ac4f0d7cbd70df7ca581d39e2e7f5620e14
Jasub_Sastre <davidjasubsastrearias@gmail.com>
2023-06-23 13:29:46 -0500

    second commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Css/styles.css'
+
+const Navbar = () => {
+    return(
+        <header className='header'>
+            <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Tienda</li>
+                    <li>Musica</li>
+                    <li>Nosotros</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
